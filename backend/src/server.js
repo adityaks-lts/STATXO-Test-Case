95dd50257a7bd32af23dd9a1931e095a93341db0
adityaks-lts <aditya432001@gmail.com>
2024-07-05 06:04:49 +0530

    try

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ const port = process.env.PORT
 
 app.use(express.json());
  
-app.use(cors({origin:[process.env.FRONTEND_URL,"http://localhost:5173"]}))
+app.use(cors({origin:[process.env.FRONTEND_URL]}))
 
 app.use("/users",userRoutes);
 app.use("/data",auth,morgan('{"request": ":request" , "id": ":id" , "data": ":date"}', {skip:(req)=>{
